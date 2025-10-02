32d7881dbf26dc457586afb76849e5d7bdd6721c
leo6103 <khoinguyen6103@gmail.com>
2025-10-02 15:14:35 +0700

    Init

---
+++ src/api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from ray import serve
+
+app = FastAPI()
+
+
+@serve.deployment(ray_actor_options={"num_cpus": 0.25})
+@serve.ingress(app)
+class RayExampleApp:
+    @app.get("/healthz")
+    async def healthz(self) -> dict[str, str]:
+        return {"status": "ok"}
+
+    @app.get("/helloworld")
+    async def helloworld(self) -> dict[str, str]:
+        return {"message": "helloworld"}
+
+
+ray_example_app = RayExampleApp.bind()
